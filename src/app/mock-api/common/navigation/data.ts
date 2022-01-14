904e4fe67112329f2faa58ce9c54b61135070428
dcalvocasanova <dcalvocasanova@gmail.com>
2022-01-13 20:07:14 -0600

    First Commit

---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,46 @@
+/* tslint:disable:max-line-length */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'profile',
+        title: 'Profile',
+        type : 'basic',
+        icon : 'iconsmind:user',
+        link : '/profile'
+    },
+    {
+        id   : 'enterprise',
+        title: 'Empresa',
+        type : 'basic',
+        icon : 'iconsmind:office',
+        link : '/enterprise'
+    }
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
